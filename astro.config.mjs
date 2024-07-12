97deb240e59b95d5db27fc3afb3b5b02d03baa7d
anasak-dev <anasniazi94@gmail.com>
2024-07-12 08:24:35 +0500

    project upgrade -astro -tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 import { resolve } from "path";
 
 // https://astro.build/config
+
+// https://astro.build/config
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,14 +19,15 @@ export default defineConfig({
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
-    compress(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    compress(),
   ],
-
   vite: {
     build: {
       rollupOptions: {
@@ -41,9 +43,7 @@ export default defineConfig({
             }
             return `assets/${extType}/[name]-[hash][extname]`;
           },
-
           chunkFileNames: "assets/js/[name]-[hash].js",
-
           entryFileNames: "assets/js/index.js",
         },
       },
